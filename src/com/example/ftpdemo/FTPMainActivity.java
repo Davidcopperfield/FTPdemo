31a4780ee113b5eb3932bd9238a202e83168dc71
davidcopperfield <familydai7@gmail.com>
2014-06-04 09:34:02 +0800

    Initial

---
+++ src/com/example/ftpdemo/FTPMainActivity.java
@@ -0,0 +1,28 @@
+package com.example.ftpdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+
+import android.os.Bundle;  
+import android.os.StrictMode;  
+import android.app.Activity;  
+import android.support.v4.widget.SimpleCursorAdapter.ViewBinder;  
+import android.view.Menu;  
+import android.view.View;  
+import android.view.View.OnClickListener;  
+import android.widget.Button;  
+import android.widget.Toast;  
+
+
+public class FTPMainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ftpmain);
+	}
+}
